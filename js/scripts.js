a60a200dac4ba4393d88f2f9afa7887370de018d
Jaki Widby <jaki.widby@gmail.com>
2020-01-24 12:05:48 -0800

    add attempt to create if statement that would replace string items containing 1 with beep

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,25 @@ $(document).ready(function() {
 
     var originalNumber = parseInt($("input#userInput").val());
     var numberList = [];
+    var newnumberList = numberlist.toString();
     var i;
 
     for (var i = 0; i <= originalNumber; i++) {
-      if (numberList.includes("1")) {
-        (result.push("Beep!"));
-      } else {
-        (numberList.push(i));
-      }
+      // if (i === 1) {
+      //   (numberList.push("Beep!"));
+      // } else {
+      (numberList.push(i));
+      // }
+      // $(".output").text(numberList);
+    }
+
+    if (newNumberList.includes("1")) {
+      (newNumberList.replace(/1/gi,"Beep!"));
       $(".output").text(numberList);
-      }
+
+    }
+
+
     event.preventDefault();
   });
 });
