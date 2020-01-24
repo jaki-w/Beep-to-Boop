f69e37ef339fca557f5292e712b6e4c935558f41
Jaki Widby <jaki.widby@gmail.com>
2020-01-24 09:18:03 -0800

    create click function eventthat returns input number to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-  $(button).click(function(event) {
+  $("button").click(function(event) {
     event.preventDefault();
-    
+
+    var originalNumber = $("input#userInput").val();
+    $(".output").text(originalNumber);
   });
 });
