d7d50d68954dbf3350fa0850c198aca2aec93de8
Jaki Widby <jaki.widby@gmail.com>
2020-01-24 12:22:30 -0800

    create function for backend logic and separate from front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,19 @@
+function translate(inputNumber) {
+  var originalNumber = parseInt($("input#userInput").val());
+  var numberList = [];
+  var i;
+
+  for (var i = 0; i <= originalNumber; i++) {
+    var stringNumber = i.toString();
+
+
+
+    $(".output").text(numberList);
+  }
+
+
 $(document).ready(function() {
   $("button").click(function(event) {
-
-
-    var originalNumber = parseInt($("input#userInput").val());
-    var numberList = [];
-    var newnumberList = numberlist.toString();
-    var i;
-
-    for (var i = 0; i <= originalNumber; i++) {
-      // if (i === 1) {
-      //   (numberList.push("Beep!"));
-      // } else {
-      (numberList.push(i));
-      // }
-      // $(".output").text(numberList);
-    }
-
-    if (newNumberList.includes("1")) {
-      (newNumberList.replace(/1/gi,"Beep!"));
-      $(".output").text(numberList);
-
-    }
-
-
     event.preventDefault();
   });
 });
