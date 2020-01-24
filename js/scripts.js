8291db7257f278a30c9639b9759eedaf43c630c5
Jaki Widby <jaki.widby@gmail.com>
2020-01-24 13:29:05 -0800

    create front end logic that outputs results of backend function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 function translate(inputNumber) {
-  var originalNumber = parseInt($("input#userInput").val());
   var numberList = [];
   var i;
 
-  for (var i = 0; i <= originalNumber; i++) {
+  for (var i = 0; i <= inputNumber; i++) {
     var stringNumber = i.toString();
 
     if (stringNumber.includes("1")) {
       numberList.push("Beep!");
     } else {
-      result.push(stringNumber + ", ");
+      numberList.push(stringNumber + ", ");
     }
   }
-
-  $(".output").text(numberList);
-  }
-
+  return numberList;
+}
 
 $(document).ready(function() {
   $("button").click(function(event) {
     event.preventDefault();
+
+    var originalNumber = parseInt($("input#userInput").val());
+    var finalOutput = translate(originalNumber);
+
+    $(".output").text(finalOutput);
   });
 });
